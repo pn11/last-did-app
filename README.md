3ae23deac11a246e5d240bbca758490a31b5b164
oka <pn11@users.noreply.github.com>
2022-06-28 00:08:03 +0900

    Add message.content

---
+++ README.md
@@ -0,0 +1,9 @@
+# Last Did App
+
+```sh
+ionic serve
+```
+
+## Reference
+
+- [Ionic/Vueでスマホアプリを作る本](https://zenn.dev/abalol/books/7b59450ff5a760)
--- src/data/messages.ts
+++ src/data/messages.ts
@@ -3,6 +3,7 @@ export interface Message {
   subject: string;
   date: string;
   id: number;
+  content: string;
 }
 
 const messages: Message[] = [
@@ -10,50 +11,57 @@ const messages: Message[] = [
     fromName: 'Matt Chorsey',
     subject: 'New event: Trip to Vegas',
     date: '9:32 AM',
-    id: 0
+    id: 0,
+    content: 'test message'
   },
   {
     fromName: 'Lauren Ruthford',
     subject: 'Long time no chat',
     date: '6:12 AM',
-    id: 1
+    id: 1,
+    content: 'test message'
   },
   {
     fromName: 'Jordan Firth',
     subject: 'Report Results',
     date: '4:55 AM',
-    id: 2
-
+    id: 2,
+    content: 'test message'
   },
   {
     fromName: 'Bill Thomas',
     subject: 'The situation',
     date: 'Yesterday',
-    id: 3
+    id: 3,
+    content: 'test message'
   },
   {
     fromName: 'Joanne Pollan',
     subject: 'Updated invitation: Swim lessons',
     date: 'Yesterday',
-    id: 4
+    id: 4,
+    content: 'test message'
   },
   {
     fromName: 'Andrea Cornerston',
     subject: 'Last minute ask',
     date: 'Yesterday',
-    id: 5
+    id: 5,
+    content: 'test message'
   },
   {
     fromName: 'Moe Chamont',
     subject: 'Family Calendar - Version 1',
     date: 'Last Week',
-    id: 6
+    id: 6,
+    content: 'test message'
   },
   {
     fromName: 'Kelly Richardson',
     subject: 'Placeholder Headhots',
     date: 'Last Week',
-    id: 7
+    id: 7,
+    content: 'test message'
   }
 ];
 
